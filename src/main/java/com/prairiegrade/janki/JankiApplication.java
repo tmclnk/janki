98f6de4600ac38a6e875b7b965380635f21ce68e
tom mclaughlin <tom@prairiegrade.com>
2026-01-14 09:11:08 -0600

    Initialize Spring Boot WebFlux project with Maven setup
    Added the initial project structure for a Spring Boot application using WebFlux, including Maven configuration (`pom.xml`), a main application class, a test class, and necessary configuration files. Set up `.gitignore` and Maven wrapper scripts for smoother project management.

---
+++ src/main/java/com/prairiegrade/janki/JankiApplication.java
@@ -0,0 +1,13 @@
+package com.prairiegrade.janki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JankiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prairiegrade/janki/JankiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prairiegrade.janki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
